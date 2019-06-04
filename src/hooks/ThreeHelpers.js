f70fa8412452344049836e21398a88c29b468d9a
Austin Brunkhorst <contact@austinbrunkhorst.com>
2019-06-03 18:16:09 -0700

    Drop target progress

--- src/hooks/ThreeHelpers.js
+++ src/hooks/ThreeHelpers.js
@@ -1,32 +1,37 @@
-import React from "react";
-// import { useDebouncedCallback } from "use-debounce";
+import * as THREE from "three";
+import { useEffect } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
-// function useLocalStorageRef(key, initialValue = null, debounce = 1000) {
-//   const valueInStorage = localStorage.getItem(key);
+function useLocalStorageRef(key, initialValue = null, debounce = 1000) {
+  const valueInStorage = localStorage.getItem(key);
 
-//   const currentValue =
-//     valueInStorage === null ? initialValue : JSON.parse(valueInStorage);
+  const currentValue =
+    valueInStorage === null ? initialValue : JSON.parse(valueInStorage);
 
-//   const [persistValue] = useDebouncedCallback(state => {
-//     console.log(`saving ${key}`);
-//     localStorage.setItem(key, JSON.stringify(state));
-//   }, debounce);
+  const [persistValue] = useDebouncedCallback(state => {
+    console.log(`saving ${key}`);
+    localStorage.setItem(key, JSON.stringify(state));
+  }, debounce);
 
-//   return [currentValue, persistValue];
-// }
-
-export function usePersistedObject(name) {
-  //const [] = useLocalStorageRef(name, null, 500);
-
-  return [];
-  // return [
-  //   serializedState === null ? defaultFactory() : deserializedObject,
-  //   function saveObject(object) {
-  //     persistState(object.toJSON());
-  //   }
-  // ];
+  return [currentValue, persistValue];
 }
 
-export function PersistedObject3D({ name, renderObject, ...props }) {
-  return <renderObject {...props} />;
+export function usePersistedObject(name, object, recursive = true) {
+  const [serializedState, persistState] = useLocalStorageRef(name, null, 500);
+
+  useEffect(() => {
+    if (object && serializedState !== null) {
+      const loader = new THREE.ObjectLoader();
+
+      const serializedObject = loader.parse(serializedState);
+
+      object.copy(serializedObject, recursive);
+    }
+  }, [object, serializedState]);
+
+  return [
+    function saveObject(object) {
+      persistState(object.toJSON());
+    }
+  ];
 }
