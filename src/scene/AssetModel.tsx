fea758c3840822ea79a5647182d5e45970dd6758
Austin Brunkhorst <AustinBrunkhorst@users.noreply.github.com>
2019-06-07 06:32:50 -0700

    Model cache progress

--- src/scene/AssetModel.tsx
+++ src/scene/AssetModel.tsx
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import React from "react";
+import React, { useMemo } from "react";
 import { useState, useEffect } from "react";
 import { GLTFLoader, GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
@@ -10,35 +10,84 @@ export interface ModelAssetProps {
   position: THREE.Vector3;
 }
 
+const modelCache = {};
+
+function fetchModel(path: string): Promise<THREE.Object3D> {
+  const loader = new GLTFLoader();
+
+  return new Promise((resolve, reject) => {
+    // Load a glTF resource
+    loader.load(path,
+      // called when the resource is loaded
+      function onModelLoad(gltf: GLTF) {
+        const [model] = gltf.scene.children;
+
+        model.updateMatrix();
+
+        const mesh = model as THREE.Mesh;
+
+        mesh.geometry.applyMatrix(model.matrix);
+
+        mesh.position.set(0, 0, 0);
+        mesh.rotation.set(0, 0, 0);
+        mesh.scale.set(1, 1, 1);
+        mesh.updateMatrix();
+
+        resolve(model.clone());
+      },
+      function onModelProgress(e: ProgressEvent) {
+        console.debug('fetchModel progress', path, e);
+      },
+      function onModelError(e: ErrorEvent) {
+        console.error(`fetchModel load failed`, path, e);
+
+        reject(e);
+      }
+    );
+  });
+}
+
+async function loadModelAsset({ guid, model }: AssetMetadata) {
+  const cacheEntry = modelCache[guid];
+
+  if (cacheEntry) {
+    return Promise.resolve(cacheEntry);
+  }
+
+  const modelObject = await fetchModel(model);
+
+  modelCache[guid] = modelObject;
+
+  return modelObject;
+}
+
 export default function ModelAsset({ asset, position }: ModelAssetProps) {
   const [model, setModel] = useState<THREE.Object3D>();
 
   useEffect(() => {
-    // Instantiate a loader
-    const loader = new GLTFLoader();
-
-    // Load a glTF resource
-    loader.load(
-      // resource URL
-      asset.model,
-      // called when the resource is loaded
-      function onModelLoad(gltf: GLTF) {
-        setModel(gltf.scene.children[0]);
+    const load = async () => {
+      try {
+        setModel(await loadModelAsset(asset));
       }
-    );
+      catch (e) {
+        console.error(ModelAsset);
+      }
+    };
+
+    load();
   }, [asset]);
 
   return model ? (
     <primitive object={model} position={position} castShadow receiveShadow />
   ) : (
-    <mesh position={position} castShadow receiveShadow>
-      <boxBufferGeometry attach="geometry" args={[0.25, 0.25, 0.25]} />
-      <meshStandardMaterial
-        attach="material"
-        color="red"
-        emissive="red"
-        emissiveIntensity={10}
-      />
-    </mesh>
-  );
+      <mesh position={position} castShadow receiveShadow>
+        <boxBufferGeometry attach="geometry" args={[0.25, 0.25, 0.25]} />
+        <meshStandardMaterial
+          attach="material"
+          color="red"
+          emissive="red"
+          emissiveIntensity={10}
+        />
+      </mesh>
+    );
 }
